4f7077688676169318c0ccde03b1b4fe69c6aeb7
HarshAditya23 <34307686+HarshAditya23@users.noreply.github.com>
2025-08-27 23:00:53 +0530

    Update MAT Plotter.py

--- MAT Plotter.py
+++ MAT Plotter.py
@@ -5,33 +5,26 @@ Created on Sat Jul 12 22:41:47 2025
 @author: gaurh
 """
 
-
-import h5py as h5 #From v7.3 matlab files
-
-data = h5.File(r'Houston13.mat')['ori_data']
-# print(mat_v7.keys())
-# #print(type(mat_v7))
-# print(len(mat_v7))
-print(data.shape)
-# from scipy.io import loadmat
+import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
+import earthpy.plot as ep
 
-# data = loadmat(r'C:\Users\gaurh\Desktop\cube2.mat')['data']
-# #gt = loadmat('GT.mat')['gt']
+data = sio.loadmat(r'sub.mat')
+print(data.keys())
+print(type(data))
 
-
-
-print(f"Data Shape: {data.shape[1:]}\nNumber of Bands: {data.shape[0]}")
+data = data['HSI']
+print(data.shape)
+print(f"Data Shape: {data.shape[1:]}\nNumber of Bands: {data.shape[2]}")
 
 fig = plt.figure(figsize = (20, 12))
-
 ax = fig.subplots(2, 5)
 
 for i in range(2):
   for j in range(5):
-    c = np.random.randint(45)
-    ax[i][j].imshow(data[c,:,:], cmap='nipy_spectral')
+    c = np.random.randint(23)
+    ax[i][j].imshow(data[:,:,c], cmap='nipy_spectral')
     ax[i][j].axis('off')
     ax[i][j].title.set_text(f"Band - {c}")
     c += 1
@@ -40,16 +33,5 @@ plt.tight_layout()
 plt.show()
 
 mdata = np.moveaxis(data, -1, 0)
-
-ep.plot_rgb(mdata, (29, 23, 2), figsize=(15, 15))
-
+ep.plot_rgb(mdata, (14, 4, 1), figsize=(15, 15))
 plt.show()
-
-def plot_data(data):
-  fig = plt.figure(figsize=(12, 12))
-  plt.imshow(data, cmap='nipy_spectral')
-  plt.colorbar()
-  plt.axis('on')
-  plt.show()
-
-plot_data(gt)
